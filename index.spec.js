3845da3d5b702477d38745e058b72c67b38694d3
chenaski <mail2chenaski@gmail.com>
2021-07-11 12:19:56 +0300

    Add ESLint

--- index.spec.js
+++ index.spec.js
@@ -1,23 +1,23 @@
-import { random } from "./index";
+import { random } from './index'
 
-test("works", () => {
+test('works', () => {
   Array(100)
     .fill(0)
-    .map(() => {
-      const result = random({ min: 1, max: 10 });
+    .forEach(() => {
+      const result = random({ min: 1, max: 10 })
 
-      expect(result).toBeGreaterThanOrEqual(1);
-      expect(result).toBeLessThanOrEqual(10);
-    });
-});
+      expect(result).toBeGreaterThanOrEqual(1)
+      expect(result).toBeLessThanOrEqual(10)
+    })
+})
 
-test("default `min` value is `0`", () => {
+test('default `min` value is `0`', () => {
   Array(100)
     .fill(0)
-    .map(() => {
-      const result = random({ max: 10 });
+    .forEach(() => {
+      const result = random({ max: 10 })
 
-      expect(result).toBeGreaterThanOrEqual(0);
-      expect(result).toBeLessThanOrEqual(10);
-    });
-});
+      expect(result).toBeGreaterThanOrEqual(0)
+      expect(result).toBeLessThanOrEqual(10)
+    })
+})
